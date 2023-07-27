5114b828ed47489099e6053b7507118e0ec1069d
Igor <igorlfs@ufmg.br>
2023-07-27 01:52:38 -0300

    proof of concept to draw shapes

--- snake-game/src/game/snake/systems.rs
+++ snake-game/src/game/snake/systems.rs
@@ -1,25 +1,52 @@
-use crate::game::snake::components::*;
-use crate::game::snake::SNAKE_BODY_PART_SIZE;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
-use bevy_prototype_lyon::prelude::*;
 
 pub fn spawn_snake(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window: &Window = window_query.get_single().unwrap();
-    let shape = shapes::RegularPolygon {
-        sides: 4,
-        feature: shapes::RegularPolygonFeature::Radius(SNAKE_BODY_PART_SIZE),
-        ..shapes::RegularPolygon::default()
-    };
 
-    commands.spawn((
-        ShapeBundle {
-            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-            path: GeometryBuilder::build_as(&shape),
+    commands.spawn(spawn_rect(
+        window.width() / 2.,
+        window.height() / 2.,
+        400.,
+        40.,
+        Color::GREEN,
+    ));
+
+    commands.spawn(spawn_rect(
+        window.width() / 2. + 200. - 8.,
+        window.height() / 2. + 15.,
+        15.,
+        4.,
+        Color::BLACK,
+    ));
+
+    commands.spawn(spawn_rect(
+        window.width() / 2. + 200. - 8.,
+        window.height() / 2. - 15.,
+        15.,
+        4.,
+        Color::BLACK,
+    ));
+
+    commands.spawn(spawn_rect(
+        window.width() / 2. + 200. + 12.,
+        window.height() / 2.,
+        24.,
+        7.,
+        Color::RED,
+    ));
+}
+
+/// Spawn a rectangle with the given parameters
+/// See <https://bevyengine.org/examples/2D%20Rendering/2d-shapes/>
+fn spawn_rect(x: f32, y: f32, length: f32, width: f32, color: Color) -> SpriteBundle {
+    SpriteBundle {
+        sprite: Sprite {
+            color,
+            custom_size: Some(Vec2::new(length, width)),
             ..default()
         },
-        Fill::color(Color::RED),
-        Stroke::new(Color::BLUE, 10.0),
-        Snake::default(),
-    ));
+        transform: Transform::from_translation(Vec3::new(x, y, 0.)),
+        ..default()
+    }
 }
