cd35f8e236b2a06db8d9aa496f5773422a5823d7
Igor <igorlfs@ufmg.br>
2023-08-01 04:08:35 -0300

    versão inicial do movimento

--- snake-game/src/game/snake/mod.rs
+++ snake-game/src/game/snake/mod.rs
@@ -1,9 +1,9 @@
+use crate::AppState;
 use bevy::prelude::*;
 use bevy::time::common_conditions::on_timer;
+use components::SnakeCounter;
 use std::time::Duration;
 use systems::*;
-
-use crate::AppState;
 
 pub mod components;
 mod systems;
@@ -15,7 +15,8 @@ pub struct SnakePlugin;
 
 impl Plugin for SnakePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_snake)
+        app.init_resource::<SnakeCounter>()
+            .add_systems(Startup, spawn_snake)
             .add_systems(Update, update_direction.run_if(in_state(AppState::InGame)))
             .add_systems(
                 Update,
--- snake-game/src/game/snake/systems.rs
+++ snake-game/src/game/snake/systems.rs
@@ -1,4 +1,4 @@
-use super::components::{Direction, Snake};
+use super::components::{Direction, SnakeBody, SnakeCounter, SnakeHead};
 use crate::game::fruit::systems::spawn_fruit;
 use crate::game::score::resources::Score;
 use crate::game::{fruit::components::Fruit, BLOCK_SIZE};
@@ -16,19 +16,53 @@ pub fn spawn_snake(mut commands: Commands, window_query: Query<&Window, With<Pri
                 custom_size: Some(Vec2::new(BLOCK_SIZE, BLOCK_SIZE)),
                 ..default()
             },
-            transform: Transform::from_xyz(window.width() / 2., window.height() / 2.0, 0.),
+            transform: Transform::from_xyz(window.width() / 2., window.height() / 2., 0.),
             ..default()
         },
-        Snake::default(),
+        SnakeHead,
         Direction::Left,
+    ));
+
+    // TODO fazer isso de  um jeito menos duplicado (extrair pra uma função com parâmetro)
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.75, 0.25),
+                custom_size: Some(Vec2::new(BLOCK_SIZE, BLOCK_SIZE)),
+                ..default()
+            },
+            transform: Transform::from_xyz(
+                window.width() / 2. + BLOCK_SIZE,
+                window.height() / 2.,
+                0.,
+            ),
+            ..default()
+        },
+        SnakeBody { count: 1 },
+    ));
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.25, 0.75, 0.25),
+                custom_size: Some(Vec2::new(BLOCK_SIZE, BLOCK_SIZE)),
+                ..default()
+            },
+            transform: Transform::from_xyz(
+                window.width() / 2. + 2. * BLOCK_SIZE,
+                window.height() / 2.,
+                0.,
+            ),
+            ..default()
+        },
+        SnakeBody { count: 0 },
     ));
 }
 
 pub fn update_direction(
     keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<&mut Direction, With<Snake>>,
+    mut snake_head_query: Query<&mut Direction, With<SnakeHead>>,
 ) {
-    if let Ok(mut dir) = player_query.get_single_mut() {
+    if let Ok(mut dir) = snake_head_query.get_single_mut() {
         if keyboard_input.pressed(KeyCode::H) {
             *dir = Direction::Left;
         }
@@ -45,14 +79,49 @@ pub fn update_direction(
 }
 
 pub fn sprite_movement(
-    mut sprite_position: Query<(&mut Direction, &mut Transform), With<Snake>>,
+    mut commands: Commands,
+    body_positions: Query<(Entity, &SnakeBody), With<SnakeBody>>,
+    mut head_position: Query<(&mut Direction, &mut Transform), With<SnakeHead>>,
+    mut counter: ResMut<SnakeCounter>,
     window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
     let window: &Window = window_query.get_single().unwrap();
 
-    for (logo, mut transform) in &mut sprite_position {
+    // Procuramos a última posição do corpo e removemos
+    let mut min = u32::MAX;
+    let mut tail: Option<Entity> = None;
+    for (entity, body) in body_positions.iter() {
+        if body.count < min {
+            min = body.count;
+            tail = Some(entity);
+        }
+    }
+    if let Some(tail) = tail {
+        commands.entity(tail).despawn();
+    }
+
+    for (direction, mut transform) in &mut head_position {
         let translation = &mut transform.translation;
-        match *logo {
+
+        // Spawno um novo body para ocupar a posição antiga da cabeça
+        commands.spawn((
+            SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0.25, 0.75, 0.25),
+                    custom_size: Some(Vec2::new(BLOCK_SIZE, BLOCK_SIZE)),
+                    ..default()
+                },
+                transform: Transform::from_xyz(translation.x, translation.y, 0.),
+                ..default()
+            },
+            SnakeBody {
+                count: counter.count,
+            },
+        ));
+        counter.count += 1;
+
+        // Movo a cabeça
+        match *direction {
             Direction::Up => {
                 if translation.y + BLOCK_SIZE < window.height() {
                     translation.y += BLOCK_SIZE
@@ -79,15 +148,17 @@ pub fn sprite_movement(
 
 pub fn handle_eat_fruit(
     mut commands: Commands,
-    mut snake_query: Query<&Transform, With<Snake>>,
+    mut snake_head_query: Query<&Transform, With<SnakeHead>>,
+    snake_body_query: Query<(&Transform, &SnakeBody), With<SnakeBody>>,
     fruit_query: Query<(Entity, &Transform), With<Fruit>>,
     window_query: Query<&Window, With<PrimaryWindow>>,
-    score: Res<Score>,
+    mut score: ResMut<Score>,
+    mut counter: ResMut<SnakeCounter>,
 ) {
-    if let Ok(snake_transform) = snake_query.get_single_mut() {
+    if let Ok(head_transform) = snake_head_query.get_single_mut() {
         if let Ok((fruit_entity, fruit_transform)) = fruit_query.get_single() {
             if collide(
-                snake_transform.translation,
+                head_transform.translation,
                 Vec2::new(40.0, 40.0),
                 fruit_transform.translation,
                 Vec2::new(40.0, 40.0),
@@ -95,10 +166,39 @@ pub fn handle_eat_fruit(
             .is_some()
             {
                 commands.entity(fruit_entity).despawn();
+                score.value += 1;
 
-                // possivelmente isso aqui pode virar uma funcao
-                // spawn de nova frutinha
-                // cor aleatoria / fruta aleatoria
+                // spawna um novo rabo
+                let mut min = u32::MAX;
+                let mut tail: Option<&Transform> = None;
+                for (transform, body) in snake_body_query.iter() {
+                    if body.count < min {
+                        min = body.count;
+                        tail = Some(transform);
+                    }
+                }
+                // Um jeito melhor de fazer isso seria simplemente não despawnar o rabo no
+                // movimento. Dá pra fazer isso usando o is_changed do Res<Score> na função de
+                // movimento.
+                if let Some(tail) = tail {
+                    let translation = tail.translation;
+                    commands.spawn((
+                        SpriteBundle {
+                            sprite: Sprite {
+                                color: Color::rgb(0.25, 0.75, 0.25),
+                                custom_size: Some(Vec2::new(BLOCK_SIZE, BLOCK_SIZE)),
+                                ..default()
+                            },
+                            transform: Transform::from_xyz(translation.x, translation.y, 0.),
+                            ..default()
+                        },
+                        SnakeBody {
+                            count: counter.count,
+                        },
+                    ));
+                    counter.count += 1;
+                }
+
                 spawn_fruit(commands, window_query);
             }
         }
