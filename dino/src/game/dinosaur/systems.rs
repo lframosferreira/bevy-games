96c460948bbea2073a459f207ac422ab2636a66f
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-08-03 21:07:38 -0300

    adding collision to floor

--- dino/src/game/dinosaur/systems.rs
+++ dino/src/game/dinosaur/systems.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
+use bevy::sprite::collide_aabb::collide;
+use bevy::window::PrimaryWindow;
 
 use super::components::Dinosaur;
 use super::resources::DinoVerticalMovement;
@@ -6,6 +8,7 @@ use super::{
     DINO_HEIGHT, DINO_INITIAL_VERTICAL_SPEED, DINO_INITIAL_Y_POS, DINO_WIDTH, DINO_X_POS, GRAVITY,
 };
 use crate::game::floor::components::Floor;
+use crate::game::floor::FLOOR_HEIGHT;
 
 pub fn spawn_dinosaur(mut commands: Commands) {
     commands.spawn((
@@ -24,24 +27,35 @@ pub fn spawn_dinosaur(mut commands: Commands) {
 
 pub fn handle_jump(
     mut dinosaur_query: Query<&mut Transform, With<Dinosaur>>,
-    floor_query: Query<&Transform, With<Floor>>,
+    floor_query: Query<&Transform, (With<Floor>, Without<Dinosaur>)>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
     keyboard_input: Res<Input<KeyCode>>,
     mut dino_vertical_movement: ResMut<DinoVerticalMovement>,
 ) {
-    if let Ok(mut dinosaur_transform) = dinosaur_query.get_single_mut() {
-        if dino_vertical_movement.moving {
-            if dinosaur_transform.translation.y == DINO_INITIAL_Y_POS {
-                dino_vertical_movement.moving = false;
-                dino_vertical_movement.speed = DINO_INITIAL_VERTICAL_SPEED;
+    let window: &Window = window_query.get_single().unwrap();
+    if let Ok(floor_transform) = floor_query.get_single() {
+        if let Ok(mut dinosaur_transform) = dinosaur_query.get_single_mut() {
+            if dino_vertical_movement.moving {
+                if collide(
+                    floor_transform.translation,
+                    Vec2::new(window.width(), FLOOR_HEIGHT),
+                    dinosaur_transform.translation,
+                    Vec2::new(DINO_WIDTH, DINO_HEIGHT),
+                )
+                .is_some()
+                {
+                    dino_vertical_movement.moving = false;
+                    dino_vertical_movement.speed = DINO_INITIAL_VERTICAL_SPEED;
+                } else {
+                    dinosaur_transform.translation.y += dino_vertical_movement.speed;
+                    dino_vertical_movement.speed += GRAVITY;
+                }
             } else {
-                dinosaur_transform.translation.y += dino_vertical_movement.speed;
-                dino_vertical_movement.speed += GRAVITY;
-            }
-        } else {
-            if keyboard_input.pressed(KeyCode::Space) {
-                dino_vertical_movement.moving = true;
-                dinosaur_transform.translation.y += dino_vertical_movement.speed;
-                dino_vertical_movement.speed += GRAVITY;
+                if keyboard_input.pressed(KeyCode::Space) {
+                    dino_vertical_movement.moving = true;
+                    dinosaur_transform.translation.y += dino_vertical_movement.speed;
+                    dino_vertical_movement.speed += GRAVITY;
+                }
             }
         }
     }
