51ba1cb14f93f565dd34ec44cecee6a3bed683f3
Igor <igorlfs@ufmg.br>
2023-08-06 07:15:02 -0300

    CI and minor changes
    - extract snake colors to consts
    - remove some type annotations and fix some typos
    - update README and add LICENSE
    - format on commit with pre-commit
    - build and test on push and pull_request
    - clippy on pull_request

--- common/src/systems.rs
+++ common/src/systems.rs
@@ -1,7 +1,7 @@
 use bevy::{prelude::*, window::PrimaryWindow};
 
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
-    let window: &Window = window_query.get_single().unwrap();
+    let window = window_query.get_single().unwrap();
     commands.spawn(Camera2dBundle {
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 1.0),
         ..default()
