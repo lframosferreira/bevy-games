49d5c1fd667452072c22b16c6424fc47318fdf27
Igor <igorlfs@ufmg.br>
2023-08-10 08:41:59 -0300

    chore(flappy): update background color

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -7,6 +7,7 @@ use game::{GamePlugin, WINDOW_X, WINDOW_Y};
 fn main() {
     App::new()
         .insert_resource(Msaa::default())
+        .insert_resource(ClearColor(Color::CYAN))
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 resolution: (WINDOW_X, WINDOW_Y).into(),
