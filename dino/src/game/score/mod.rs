5dd35bd51d67e80d1e0372bf4a9b77183166ba7b
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-08-10 14:40:46 -0300

    adicionei spawn do score

--- dino/src/game/score/mod.rs
+++ dino/src/game/score/mod.rs
@@ -12,7 +12,11 @@ impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<Score>()
             .init_resource::<ScoreUpdateTimer>()
-            .add_systems(Update, (tick_score_update_timer, update_score))
+            .add_systems(Startup, spawn_score_text)
+            .add_systems(
+                Update,
+                (tick_score_update_timer, update_score, update_score_text),
+            )
             .add_systems(OnEnter(AppState::InGame), insert_score)
             .add_systems(OnExit(AppState::GameOver), reset_score);
     }
--- dino/src/game/score/systems.rs
+++ dino/src/game/score/systems.rs
@@ -1,5 +1,5 @@
 use crate::game::score::resources::*;
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 pub fn insert_score(mut commands: Commands) {
     commands.insert_resource(Score::default());
@@ -16,5 +16,55 @@ pub fn tick_score_update_timer(mut score_update_timer: ResMut<ScoreUpdateTimer>,
 pub fn update_score(mut score: ResMut<Score>, score_update_timer: Res<ScoreUpdateTimer>) {
     if score_update_timer.timer.finished() {
         score.value += 1
+    }
+}
+
+#[derive(Component, Default)]
+pub struct ScoreText;
+
+pub fn spawn_score_text(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window: &Window = window_query.get_single().unwrap();
+    commands.spawn((
+        Text2dBundle {
+            text: Text::from_section(
+                "Score: 00000",
+                TextStyle {
+                    color: Color::BLACK,
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 20.0,
+                },
+            ),
+            transform: Transform::from_xyz(window.width() - 80.0, window.height() - 20.0, 0.0),
+            ..default()
+        },
+        ScoreText::default(),
+    ));
+}
+
+pub fn update_score_text(
+    mut commands: Commands,
+    score_text_query: Query<Entity, With<ScoreText>>,
+    score: Res<Score>,
+    asset_server: Res<AssetServer>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window: &Window = window_query.get_single().unwrap();
+    if let Ok(score_text_entity) = score_text_query.get_single() {
+        commands.entity(score_text_entity).insert(Text2dBundle {
+            text: Text::from_section(
+                format!("Score: {:0>5}", score.value.to_string()),
+                TextStyle {
+                    color: Color::BLACK,
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 20.0,
+                },
+            ),
+            transform: Transform::from_xyz(window.width() - 80.0, window.height() - 20.0, 0.0),
+            ..default()
+        });
     }
 }
