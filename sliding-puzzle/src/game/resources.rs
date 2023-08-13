c2a1805cd089abedeb20f25f83eaed75cbb95282
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-08-13 20:41:32 -0300

    desenho da configuração inicial randomizada

--- sliding-puzzle/src/game/resources.rs
+++ sliding-puzzle/src/game/resources.rs
@@ -1,36 +1,39 @@
 use bevy::prelude::*;
-use ndarray::prelude::*;
 use rand::prelude::*;
 
-pub fn is_outside_grid(pos: (i8, i8)) -> bool {
-    true
+pub fn is_outside_grid(pos: (i32, i32)) -> bool {
+    pos.0 < 0 || pos.0 > 3 || pos.1 < 0 || pos.1 > 3
 }
 
-pub fn get_starting_grid(number_of_shuffles: u32) -> Array2<u8> {
-    let mut matrix: Array2<u8> = Array::from_shape_vec(
-        (4, 4),
-        vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0],
-    )
-    .unwrap();
-    let mut empty_pos: (u8, u8) = (0, 0);
+pub fn get_starting_grid(number_of_shuffles: u32) -> [[i32; 4]; 4] {
+    let mut matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
+    let mut empty_pos: (i32, i32) = (3, 3);
     for _ in 0..number_of_shuffles {
-        let movements = [(0, 1), (0, -1), (1, 0), (-1, 0)];
+        let mut movements = [(0, 1), (0, -1), (1, 0), (-1, 0)];
         movements.shuffle(&mut thread_rng());
-        for movement in movements.iter() {}
+        for movement in movements.iter() {
+            let future_pos = (empty_pos.0 + movement.0, empty_pos.1 + movement.1);
+            if !is_outside_grid(future_pos) {
+                matrix[empty_pos.0 as usize][empty_pos.1 as usize] =
+                    matrix[future_pos.0 as usize][future_pos.1 as usize];
+                matrix[future_pos.0 as usize][future_pos.1 as usize] = 0;
+                empty_pos = future_pos;
+                break;
+            }
+        }
     }
-    println!("{:?}", matrix);
     matrix
 }
 
 #[derive(Resource)]
 pub struct GridStatus {
-    pub matrix: Array2<u8>,
+    pub matrix: [[i32; 4]; 4],
 }
 
 impl Default for GridStatus {
     fn default() -> Self {
         Self {
-            matrix: get_starting_grid(20),
+            matrix: get_starting_grid(50),
         }
     }
 }
--- sliding-puzzle/src/game/systems.rs
+++ sliding-puzzle/src/game/systems.rs
@@ -1,21 +1,34 @@
 use super::resources::*;
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 const NUMBER_SIZE: f32 = 80.0;
 
-pub fn spawn_blocks(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((Text2dBundle {
-        text: Text::from_section(
-            "12",
-            TextStyle {
-                color: Color::WHITE,
-                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-                font_size: NUMBER_SIZE,
-            },
-        ),
-        transform: Transform::from_xyz(100.0, 100.0, 0.0),
-        ..default()
-    },));
+pub fn spawn_blocks(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    grid_status: Res<GridStatus>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window: &Window = window_query.get_single().unwrap();
+    for i in 0..4 {
+        for j in 0..4 {
+            let transform_x: f32 = window.width() * (i as f32 / 4.0) + window.width() / 8.0;
+            let transform_y: f32 = window.height() * (j as f32 / 4.0) + window.height() / 8.0;
+            commands.spawn((Text2dBundle {
+                text: Text::from_section(
+                    grid_status.matrix[i][j].to_string(),
+                    TextStyle {
+                        color: Color::WHITE,
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: NUMBER_SIZE,
+                    },
+                ),
+                transform: Transform::from_xyz(transform_x, transform_y, 0.0),
+                ..default()
+            },));
+        }
+    }
 }
 
 pub fn insert_grid_status(mut commands: Commands) {
@@ -23,5 +36,5 @@ pub fn insert_grid_status(mut commands: Commands) {
 }
 
 pub fn reset_grid_status(mut grid_status: ResMut<GridStatus>) {
-    grid_status.matrix = get_starting_grid(20);
+    grid_status.matrix = get_starting_grid(50);
 }
