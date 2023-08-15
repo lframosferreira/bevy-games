83bf3be0166669e521db90c82e38aea8cb788e0d
Igor <igorlfs@ufmg.br>
2023-08-15 11:20:24 -0300

    fix(snake): remove sound effects

--- snake/src/systems.rs
+++ snake/src/systems.rs
@@ -1,7 +1,6 @@
 use crate::game::BLOCK_SIZE;
 use bevy::prelude::*;
 use bevy_prototype_debug_lines::*;
-use common::events::EndGame;
 
 pub const WINDOW_X: f32 = 1000.0;
 pub const WINDOW_Y: f32 = 600.0;
@@ -20,19 +19,5 @@ pub fn draw_grid(mut lines: ResMut<DebugLines>) {
             Vec3::new(WINDOW_X, BLOCK_SIZE * i as f32, 0.),
             0.0,
         )
-    }
-}
-
-pub fn death_sound_effect(
-    mut game_over_event_reader: EventReader<EndGame>,
-    asset_server: Res<AssetServer>,
-    mut commands: Commands,
-) {
-    for _ in game_over_event_reader.iter() {
-        let sound_effect = asset_server.load("audio/lego-yoda-death-sound-effect.ogg");
-        commands.spawn(AudioBundle {
-            source: sound_effect,
-            ..default()
-        });
     }
 }
